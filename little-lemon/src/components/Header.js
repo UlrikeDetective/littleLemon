7240d28b8acd3460a67eb8e49db0dd7a14c4ff87
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-04-05 20:36:59 +0200

    logo works

--- little-lemon/src/components/Header.js
+++ little-lemon/src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <img src="little-lemon/src/logo.png" alt="Little Lemon Logo" />
+      <img src="logo.png" alt="Little Lemon Logo" />
     </header>
   );
 }
