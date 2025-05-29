b50995b80adb9c6115dc7dccffb8ca44f156ce8c
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-05-29 13:52:21 +0200

    littlelemon API V04

--- littlelemonAPI/LittleLemon/LittleLemonAPI/urls.py
+++ littlelemonAPI/LittleLemon/LittleLemonAPI/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 from . import views
 
+def home(request):
+    return HttpResponse("Welcome to the Little Lemon API!")
+
+
 urlpatterns = [
-    # Example endpoints (add your actual views as you implement them)
     path('menu-items/', views.MenuItemList.as_view(), name='menuitem-list'),
     path('categories/', views.CategoryList.as_view(), name='category-list'),
     path('orders/', views.OrderList.as_view(), name='order-list'),
+    path('', views.home, name='home'),
 ]
